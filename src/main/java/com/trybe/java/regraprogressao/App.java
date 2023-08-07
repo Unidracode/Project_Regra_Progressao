d5ecda78a01aa944ad3c1e9df8990518a3d0a465
Gustavo Alves <unidragorn@gmail.com>
2023-08-07 19:51:40 -0300

    commit inicial

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,14 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int activityNumber = scanner.nextInt();
+
+    String[] activities = new String[activityNumber];
+
+    for (int i = 0; i < activityNumber; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      activities[i] = scanner.next();
   }
 }
