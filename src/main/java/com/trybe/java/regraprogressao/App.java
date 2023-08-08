e606976a77c395f6b8546b90ab9446320dc6eed3
Gustavo Alves <unidragorn@gmail.com>
2023-08-08 17:56:37 -0300

    finaliza projeto

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -19,6 +19,8 @@ public class App {
     String[] activity = new String[activityNumber];
     int[] activityWeight = new int[activityNumber];
     int[] activityGrade = new int[activityNumber];
+    int sumWeight = 0;
+    int sumGrade = 0;
 
     for (int i = 0; i < activityNumber; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
@@ -31,6 +33,21 @@ public class App {
       System.out.println("Digite a nota obtida para " + activity[i] + ":");
       activityGrade[i] = scanner.nextInt();
       scanner.nextLine();
+
+      sumWeight += activityWeight[i];
+      sumGrade += (activityGrade[i] * activityWeight[i]);
+    }
+
+    double finalGrade = (double) sumGrade / sumWeight;
+
+    if (finalGrade >= 85) {
+      System.out.println("Parabéns! Você alcançou " + finalGrade
+          + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + finalGrade
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
   }
 }
